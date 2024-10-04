b2d8745acf74ff39f46c5ff5cffd48eac008be83
basantsharma <basant.kr.sharma.apple@gmail.com>
2024-10-04 08:07:56 +0200

    Update model deploy scripts

--- common-data-jpa/deploy/docker-compose/db/createDb.sh
+++ common-data-jpa/deploy/docker-compose/db/createDb.sh
@@ -5,7 +5,7 @@ docker rm -f mysql
 docker rm -f db-ui-mysql
 docker volume prune -f
 
-docker-compose --env-file ../.env -f docker-compose-mysql.yml up -d --force-recreate
+docker-compose --env-file ../../.env -f docker-compose-mysql.yml up -d --force-recreate
 #!/bin/bash
 
-../deleteDanglingImages.sh
+../../deleteDanglingImages.sh
